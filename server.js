ce8fbc869ff229074e019aa1e328e9bde9599c67
Brandon <booth.brandon4@gmail.com>
2017-04-11 12:13:03 -0400

    at starting point

--- server.js
+++ server.js
@@ -2,32 +2,21 @@
 
 require('dotenv').config();
 
-const PORT        = process.env.PORT || 8080;
-const ENV         = process.env.ENV || "development";
-const express     = require("express");
-const bodyParser  = require("body-parser");
-const sass        = require("node-sass-middleware");
-const methodOverride = require('method-override');
-const app         = express();
+const PORT            = process.env.PORT || 8080;
+const ENV             = process.env.ENV || "development";
+const express         = require("express");
+const bodyParser      = require("body-parser");
+const sass            = require("node-sass-middleware");
+const methodOverride  = require('method-override');
+const fs              = require('fs');
+const app             = express();
 
-const knexConfig  = require("./knexfile");
-const knex        = require("knex")(knexConfig[ENV]);
-const morgan      = require('morgan');
-const knexLogger  = require('knex-logger');
-const cookieSession = require('cookie-session');
-//const mailgun     = require('./mailgun/mailgun');
+const knexConfig      = require("./knexfile");
+const knex            = require("knex")(knexConfig[ENV]);
+const morgan          = require('morgan');
+const knexLogger      = require('knex-logger');
+const cookieSession   = require('cookie-session');
 
-// Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
-const notifyRoutes = require("./routes/notify");
-const routes = require("./routes/main");
-
-// Load the logger first so all (static) HTTP requests are logged to STDOUT
-// 'dev' = Concise output colored by response status for development use.
-//         The :status token will be colored red for server error codes, yellow for client error codes, cyan for redirection codes, and uncolored for all other codes.
-// app.use(morgan('dev'));
-
-// Log knex SQL queries to STDOUT as well
 app.use(knexLogger(knex));
 
 app.set("view engine", "ejs");
@@ -45,12 +34,14 @@ app.use(methodOverride('_method'))
 
 app.use(express.static("public"));
 
-// Mount all resource routes
-// app.use("/api/users", usersRoutes(knex));
-app.use("/notify", notifyRoutes());
-app.use("/", routes(knex));
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
+app.post("/", (req, res) => {
+});
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+ 
