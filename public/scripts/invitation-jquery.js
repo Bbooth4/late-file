6a9654c5296761dd0e16d84b6a4078cc08b80a35
Brandon <booth.brandon4@gmail.com>
2017-04-11 11:55:18 -0400

    base starting point

---
+++ public/scripts/invitation-jquery.js
@@ -0,0 +1,27 @@
+$(() => {
+  $('#first').focus();
+   
+  const list = $('.list');
+  const add = $('.add');
+  const field = $('.field');
+  let index = 2;
+
+  $('.field').keyup((e) => {
+    $('.field').last().addClass('unfaded').addClass('transition');
+    let emails = $("[type='email']").toArray();
+    if (field !== '' && emails[emails.length - 1].value) {
+      e.preventDefault(); 
+      index += 1; 
+
+      $('.field').append(`<div class="empty-space">
+          <div class='form-group field faded visible'>
+            <label class='control-label'>Invite ${index}: </label>
+            <input class='col-xs-12 col-sm-6 form-control' type="email" name="email" placeholder='email'>
+          </div>`);
+      $('.field').last().slideDown('fast', function() {
+        $('.field').first().addClass('transition').removeClass('faded').addClass('unfaded').removeClass('field');
+      $(emails[emails.length - 1]).focus();
+      })
+    }
+  });
+}); 
